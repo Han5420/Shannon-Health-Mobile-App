e520fe4161959744636810e5ab581637ba566aea
Kayeon Seon <gyseon20@duksung.ac.kr>
2022-11-07 19:13:48 -0600

    ChatBot button, layout

--- app/src/main/java/com/example/myapplication20/AddActivity.kt
+++ app/src/main/java/com/example/myapplication20/AddActivity.kt
@@ -8,6 +8,7 @@ import android.provider.MediaStore
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import com.bumptech.glide.Glide
@@ -19,80 +20,104 @@ import java.util.*
 
 class AddActivity : AppCompatActivity() {
     lateinit var binding: ActivityAddBinding
-    lateinit var filePath: String
+    // lateinit var filePath: String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         // setContentView(R.layout.activity_add)
         binding = ActivityAddBinding.inflate(layoutInflater)
         setContentView(binding.root)
-    }
 
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.menu_add, menu)
-        return super.onCreateOptionsMenu(menu)
-    }
-    val requestLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
-        if (it.resultCode === android.app.Activity.RESULT_OK) {
-            Glide
-                .with(applicationContext)
-                .load(it.data?.data)
-                .apply(RequestOptions().override(250, 200))
-                .centerCrop()
-                .into(binding.addImageView)
-            val cursor = contentResolver.query(it.data?.data as Uri,
-                arrayOf<String>(MediaStore.Images.Media.DATA), null, null, null)
-            cursor?.moveToFirst().let {
-                filePath = cursor?.getString(0) as String
+        binding.sendBtn.setOnClickListener {
+            if (binding.userAnswer.text.isNotEmpty()){
+                if (binding.userAnswer.text.equals("1")) {
+                    binding.showUserAnswer.text = "1"
+                    binding.showUserAnswer.visibility = View.VISIBLE
+                }
+                else if (binding.userAnswer.text.equals("2")) {
+                    binding.showUserAnswer.text = "2"
+                    binding.showUserAnswer.visibility = View.VISIBLE
+                }
+                else if (binding.userAnswer.text.equals("3")) {
+                    binding.showUserAnswer.text = "3"
+                    binding.showUserAnswer.visibility = View.VISIBLE
+                }
+                else if (binding.userAnswer.text.equals("4")) {
+                    binding.showUserAnswer.text = "4"
+                    binding.showUserAnswer.visibility = View.VISIBLE
+                }
+                else {
+
+                }
             }
         }
     }
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if (item.itemId === R.id.menu_add_gallery) {
-            val intent = Intent(Intent.ACTION_PICK)
-            intent.setDataAndType(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*")
-            requestLauncher.launch(intent)
-        }
-        else if (item.itemId === R.id.menu_add_save) {
-            if (binding.addImageView.drawable !== null && binding.addEditView.text.isNotEmpty()) {
-                saveStore()
-            }
-            else {
-                Toast.makeText(this, "데이터가 모두 입력되지 않았습니다.", Toast.LENGTH_SHORT).show()
-            }
-        }
-        return super.onOptionsItemSelected(item)
-    }
 
-    private fun saveStore() {
-        val data = mapOf(
-            "email" to MyApplication.email,
-            "content" to binding.addEditView.text.toString(),
-            "date" to dateToString(Date())
-        )
-
-        MyApplication.db.collection("news")
-            .add(data)
-            .addOnSuccessListener {
-                uploadImage(it.id)
-            }
-            .addOnFailureListener {
-                Log.d("mobileApp", "data save error")
-            }
-    }
-
-    private fun uploadImage(docId: String) {
-        val storage = MyApplication.storage
-        val storageRef = storage.reference
-        val imageRef = storageRef.child("images/${docId}.jpg")
-
-        val file = Uri.fromFile(File(filePath))
-        imageRef.putFile(file)
-            .addOnSuccessListener {
-                Toast.makeText(this, "save ok..", Toast.LENGTH_SHORT).show()
-                finish()
-            }
-            .addOnFailureListener {
-                Log.d("mobileApp", "file save error")
-            }
-    }
+//    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+//        menuInflater.inflate(R.menu.menu_add, menu)
+//        return super.onCreateOptionsMenu(menu)
+//    }
+//    val requestLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+//        if (it.resultCode === android.app.Activity.RESULT_OK) {
+//            Glide
+//                .with(applicationContext)
+//                .load(it.data?.data)
+//                .apply(RequestOptions().override(250, 200))
+//                .centerCrop()
+//                .into(binding.addImageView)
+//            val cursor = contentResolver.query(it.data?.data as Uri,
+//                arrayOf<String>(MediaStore.Images.Media.DATA), null, null, null)
+//            cursor?.moveToFirst().let {
+//                filePath = cursor?.getString(0) as String
+//            }
+//        }
+//    }
+//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+//        if (item.itemId === R.id.menu_add_gallery) {
+//            val intent = Intent(Intent.ACTION_PICK)
+//            intent.setDataAndType(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*")
+//            requestLauncher.launch(intent)
+//        }
+//        else if (item.itemId === R.id.menu_add_save) {
+//            if (binding.addImageView.drawable !== null && binding.addEditView.text.isNotEmpty()) {
+//                saveStore()
+//            }
+//            else {
+//                Toast.makeText(this, "데이터가 모두 입력되지 않았습니다.", Toast.LENGTH_SHORT).show()
+//            }
+//        }
+//        return super.onOptionsItemSelected(item)
+//    }
+//
+//    private fun saveStore() {
+//        val data = mapOf(
+//            "email" to MyApplication.email,
+//            "content" to binding.addEditView.text.toString(),
+//            "date" to dateToString(Date())
+//        )
+//
+//        MyApplication.db.collection("news")
+//            .add(data)
+//            .addOnSuccessListener {
+//                uploadImage(it.id)
+//            }
+//            .addOnFailureListener {
+//                Log.d("mobileApp", "data save error")
+//            }
+//    }
+//
+//    private fun uploadImage(docId: String) {
+//        val storage = MyApplication.storage
+//        val storageRef = storage.reference
+//        val imageRef = storageRef.child("images/${docId}.jpg")
+//
+//        val file = Uri.fromFile(File(filePath))
+//        imageRef.putFile(file)
+//            .addOnSuccessListener {
+//                Toast.makeText(this, "save ok..", Toast.LENGTH_SHORT).show()
+//                finish()
+//            }
+//            .addOnFailureListener {
+//                Log.d("mobileApp", "file save error")
+//            }
+//    }
 }
